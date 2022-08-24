81cb8925a525e0ec9ee3ce7d3753de4a6d01626c
Jailson Volnei dos Santos <jailsvs@gmail.com>
2022-08-24 20:36:55 -0300

    Single commit

---
+++ CalcTest/CalcUnitTest.cs
@@ -0,0 +1,88 @@
+﻿using ConsoleCalc;
+
+namespace CalcTest
+{
+    public class CalcUnitTest
+    {
+        [Fact(DisplayName = "Sum 2 numbers")]
+        [Trait("Calc", "Sum")]
+        public void Sum2Numbers()
+        {
+            //Arrange - Organizar
+            var number1 = 8.9;
+            var number2 = 10.12;
+            var expectedValue = 19.02;
+
+            //Act - Agir
+            var result = Calc.Sum(number1, number2);
+
+            //Assert - Assertir
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Fact]
+        [Trait("Calc", "Subtract")]
+        public void Subtract2Numbers()
+        {
+            //Arrange - Organizar
+            var number1 = 8.9;
+            var number2 = 10.12;
+            var expectedValue = 1.22;
+
+            //Act - Agir
+            var result = Calc.Subtract(number2, number1);
+
+            //Assert - Assertir
+            Assert.Equal(expectedValue, result, 2);
+        }
+
+        [Fact]
+        [Trait("Calc", "Divide")]
+        public void Divide2Numbers()
+        {
+            //Arrange - Organizar
+            var number1 = 18;
+            var number2 = 2;
+            var expectedValue = 9;
+
+            //Act - Agir
+            var result = Calc.Divide(number1, number2);
+
+            //Assert - Assertir
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Fact]
+        [Trait("Calc", "Multiply")]
+        public void Multiply2Numbers()
+        {
+            //Arrange - Organizar
+            var number1 = 5.5;
+            var number2 = 2;
+            var expectedValue = 11;
+
+            //Act - Agir
+            var result = Calc.Multiply(number1, number2);
+
+            //Assert - Assertir
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Theory]
+        [InlineData(1,2,3)]
+        [InlineData(-4,-6,-10)]
+        [InlineData(-2,2,0)]
+        [InlineData(int.MinValue, +1, int.MinValue+1)]
+        public void Sum2RelativeNumbers_ResultRelativeNumber(int number1, int number2, int expectedValue)
+        {
+            var result = Calc.Sum(number1, number2);
+            Assert.Equal(expectedValue, result);
+        }
+
+        [Fact(Skip = "Test not available yet")]
+        public void TestNotAvailable()
+        {
+            
+        }
+    }
+}
---
+++ ConsoleCalc/Calc.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleCalc
+{
+    public class Calc
+    {
+        public static double Sum(double numero1, double numero2)
+        {
+            return (numero1 + numero2);
+        }
+        public static double Subtract(double numero1, double numero2)
+        {
+            return (numero1 - numero2);
+        }
+        public static double Multiply(double numero1, double numero2)
+        {
+            return (numero1 * numero2);
+        }
+        public static double Divide(double numero1, double numero2)
+        {
+            return (numero1 / numero2);
+        }
+        public static bool IsEvenNumber(int numero)
+        {
+            return numero % 2 == 0;
+        }
+    }
+}
